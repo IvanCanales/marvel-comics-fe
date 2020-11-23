6623f4914550ae2ee486ded4feb5d6e6b87b801a
ivandcv <73497603+ivandcv@users.noreply.github.com>
2020-11-22 21:50:42 -0600

    rollback to http

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://ec2-3-21-246-173.us-east-2.compute.amazonaws.com:3000/",
+  baseURL: "http://ec2-3-21-246-173.us-east-2.compute.amazonaws.com:3000/",
   timeout: 10000,
 });
